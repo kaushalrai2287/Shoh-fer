f3e3025c70beb0d4df3a191ce80036a86419eaff
Kaushal Rai <kaushalraiskt111@gmail.com>
2025-05-14 15:32:54 +0530

    segments_id in sigunup

--- src/app/api/driver-professional-info/route.ts
+++ src/app/api/driver-professional-info/route.ts
@@ -1,66 +1,67 @@
 // import { supabase } from '../../lib/supabase';
-import { createClient } from '../../../../utils/supabase/client';
-import { NextResponse } from 'next/server';
+import { createClient } from "../../../../utils/supabase/client";
+import { NextResponse } from "next/server";
 
-export async function GET(req:Request) {
-    try {
-        const supabase = await createClient();
-        // Fetch years of experience
-        const { data: experience, error: experienceError } = await supabase
-            .from('driver_experience')
-            .select('*');
+export async function GET(req: Request) {
+  try {
+    const supabase = await createClient();
+    // Fetch years of experience
+    const { data: experience, error: experienceError } = await supabase
+      .from("driver_experience")
+      .select("*");
 
-        if (experienceError) throw experienceError;
+    if (experienceError) throw experienceError;
 
-        // Fetch vehicle types
-        const { data: vehicleTypes, error: vehicleError } = await supabase
-            .from('driver_vehicle_type')
-            .select('*');
+    // Fetch vehicle types
+    const { data: vehicleTypes, error: vehicleError } = await supabase
+      .from("driver_vehicle_type")
+      .select("*");
 
-        if (vehicleError) throw vehicleError;
+    if (vehicleError) throw vehicleError;
 
-        // Fetch brands
-        const { data: brands, error: brandError } = await supabase
-            .from('brands')
-            .select('id, name');
+    // Fetch brands
+    const { data: brands, error: brandError } = await supabase
+      .from("brands")
+      .select("id, name");
 
-        if (brandError) throw brandError;
+    if (brandError) throw brandError;
 
-        // Fetch driving types
-        const { data: driving, error: drivingError } = await supabase
-            .from('driver_transmission_type')
-            .select('*');
+    // Fetch driving types
+    const { data: driving, error: drivingError } = await supabase
+      .from("driver_transmission_type")
+      .select("*");
 
-        if (drivingError) throw drivingError;
+    if (drivingError) throw drivingError;
 
-        // Fetch license categories
-        const { data: licenseCategories, error: licenseCategoryError } = await supabase
-            .from('driver_license_category')
-            .select('*');
+    // Fetch license categories
+    const { data: licenseCategories, error: licenseCategoryError } =
+      await supabase.from("driver_license_category").select("*");
 
-        if (licenseCategoryError) throw licenseCategoryError;
+    if (licenseCategoryError) throw licenseCategoryError;
+    const { data: segements, error: segementsError } = await supabase
+      .from("segments")
+      .select("*");
 
-        // Fetch license expiry dates
-       
+    if (segementsError) throw segementsError;
+    // Fetch license expiry dates
 
-        // Fetch spoken languages
-   
+    // Fetch spoken languages
 
-        // Return formatted response
-        return NextResponse.json({
-            status: '1',
-            message: 'Data fetched successfully',
-            data: {
-                experience,
-                vehicleTypes,
-                brands,
-                driving,
-                licenseCategories,
-              
-            }
-        });
-    } catch (error:any) {
-        console.error('Error fetching professional info:', error.message);
-        return NextResponse.json({ status: '0', message: error.message });
-    }
+    // Return formatted response
+    return NextResponse.json({
+      status: "1",
+      message: "Data fetched successfully",
+      data: {
+        experience,
+        vehicleTypes,
+        brands,
+        driving,
+        licenseCategories,
+        segements,
+      },
+    });
+  } catch (error: any) {
+    console.error("Error fetching professional info:", error.message);
+    return NextResponse.json({ status: "0", message: error.message });
+  }
 }
--- src/app/api/drivercancelride/route.ts
+++ src/app/api/drivercancelride/route.ts
@@ -49,6 +49,7 @@ export async function POST(req:Request) {
         { status: 500 }
       );
     }
+
     
 
     return NextResponse.json(
